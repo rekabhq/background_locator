f0d3617f5e94bc8b2337d1dd9ef0457e080d3235
Gerardo Ibarra <gibarra@upgradeit.com.ar>
2020-03-13 09:31:34 -0300

    fix ident call

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -130,7 +130,7 @@ class _MyAppState extends State<MyApp> {
         }
         break;
       case PermissionStatus.granted:
-         _startLocator();
+        _startLocator();
         break;
     }
   }
