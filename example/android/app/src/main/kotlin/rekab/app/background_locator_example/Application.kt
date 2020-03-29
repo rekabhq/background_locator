2fc7db0fad4ebc60a9dac31943de9ba3773805fc
Marcelo Henrique Neppel <marcelo.neppel@gmail.com>
2020-03-29 11:40:37 -0300

    Rebase.

---
+++ example/android/app/src/main/kotlin/rekab/app/background_locator_example/Application.kt
@@ -0,0 +1,22 @@
+package rekab.app.background_locator_example
+
+import rekab.app.background_locator.BackgroundLocatorPlugin
+import rekab.app.background_locator.LocatorService
+import io.flutter.app.FlutterApplication
+import io.flutter.plugin.common.PluginRegistry
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugins.pathprovider.PathProviderPlugin
+
+class Application : FlutterApplication(), PluginRegistrantCallback {
+    override fun onCreate() {
+        super.onCreate()
+        LocatorService.setPluginRegistrant(this)
+    }
+
+    override fun registerWith(registry: PluginRegistry?) {
+        if (!registry!!.hasPlugin("io.flutter.plugins.pathprovider")) {
+            PathProviderPlugin.registerWith(registry!!.registrarFor("io.flutter.plugins.pathprovider"))
+        }
+    }
+}
