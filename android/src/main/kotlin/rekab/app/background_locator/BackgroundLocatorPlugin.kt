f776c6a4d8515a6c9e9f058b01fce9dbc05898c8
Sudhi S <sudhi.s@stabilix.com>
2022-05-13 15:50:13 +0530

    Kotline code migrations

--- android/src/main/kotlin/rekab/app/background_locator/BackgroundLocatorPlugin.kt
+++ android/src/main/kotlin/rekab/app/background_locator/BackgroundLocatorPlugin.kt
@@ -212,23 +212,25 @@ class BackgroundLocatorPlugin
     override fun onMethodCall(call: MethodCall, result: Result) {
         when (call.method) {
             Keys.METHOD_PLUGIN_INITIALIZE_SERVICE -> {
-                val args: Map<Any, Any> = call.arguments()
+                val args: Map<Any, Any>? = call.arguments()
 
                 // save callback dispatcher to use it when device reboots
-                PreferencesManager.saveCallbackDispatcher(context!!, args)
+                args?.let { PreferencesManager.saveCallbackDispatcher(context!!, it) }
 
-                initializeService(context!!, args)
+                args?.let { initializeService(context!!, it) }
                 result.success(true)
             }
             Keys.METHOD_PLUGIN_REGISTER_LOCATION_UPDATE -> {
-                val args: Map<Any, Any> = call.arguments()
+                val args: Map<Any, Any>? = call.arguments()
 
                 // save setting to use it when device reboots
-                PreferencesManager.saveSettings(context!!, args)
+                args?.let { PreferencesManager.saveSettings(context!!, it) }
 
-                registerLocator(context!!,
-                        args,
+                args?.let {
+                    registerLocator(context!!,
+                        it,
                         result)
+                }
             }
             Keys.METHOD_PLUGIN_UN_REGISTER_LOCATION_UPDATE -> {
                 unRegisterPlugin(context!!, result)
@@ -240,8 +242,8 @@ class BackgroundLocatorPlugin
                     return
                 }
 
-                val args: Map<Any, Any> = call.arguments()
-                updateNotificationText(context!!, args)
+                val args: Map<Any, Any>? = call.arguments()
+                args?.let { updateNotificationText(context!!, it) }
                 result.success(true)
             }
             else -> result.notImplemented()
@@ -263,7 +265,7 @@ class BackgroundLocatorPlugin
         channel?.setMethodCallHandler(plugin)
     }
 
-    override fun onNewIntent(intent: Intent?): Boolean {
+    override fun onNewIntent(intent: Intent): Boolean {
         if (intent?.action != Keys.NOTIFICATION_ACTION) {
             // this is not our notification
             return false
@@ -272,12 +274,15 @@ class BackgroundLocatorPlugin
         val notificationCallback = PreferencesManager.getCallbackHandle(activity!!, Keys.NOTIFICATION_CALLBACK_HANDLE_KEY)
         if (notificationCallback != null && IsolateHolderService.backgroundEngine != null) {
             val backgroundChannel =
-                    MethodChannel(IsolateHolderService.backgroundEngine?.dartExecutor?.binaryMessenger, Keys.BACKGROUND_CHANNEL_ID)
+                IsolateHolderService.backgroundEngine?.dartExecutor?.binaryMessenger?.let {
+                    MethodChannel(
+                        it, Keys.BACKGROUND_CHANNEL_ID)
+                }
             activity?.mainLooper?.let {
                 Handler(it)
                         .post {
-                            backgroundChannel.invokeMethod(Keys.BCM_NOTIFICATION_CLICK,
-                                    hashMapOf(Keys.ARG_NOTIFICATION_CALLBACK to notificationCallback))
+                            backgroundChannel?.invokeMethod(Keys.BCM_NOTIFICATION_CLICK,
+                                hashMapOf(Keys.ARG_NOTIFICATION_CALLBACK to notificationCallback))
                         }
             }
         }
@@ -298,6 +303,8 @@ class BackgroundLocatorPlugin
 
     override fun onDetachedFromActivityForConfigChanges() {
     }
+
+
 
 
 }
--- android/src/main/kotlin/rekab/app/background_locator/pluggables/InitPluggable.kt
+++ android/src/main/kotlin/rekab/app/background_locator/pluggables/InitPluggable.kt
@@ -19,12 +19,15 @@ class InitPluggable : Pluggable {
         if (!isInitCallbackCalled) {
             (PreferencesManager.getCallbackHandle(context, Keys.INIT_CALLBACK_HANDLE_KEY))?.let { initCallback ->
                 val initialDataMap = PreferencesManager.getDataCallback(context, Keys.INIT_DATA_CALLBACK_KEY)
-                val backgroundChannel = MethodChannel(IsolateHolderService.backgroundEngine?.dartExecutor?.binaryMessenger,
+                val backgroundChannel = IsolateHolderService.backgroundEngine?.dartExecutor?.binaryMessenger?.let {
+                    MethodChannel(
+                        it,
                         Keys.BACKGROUND_CHANNEL_ID)
+                }
                 Handler(context.mainLooper)
                         .post {
-                            backgroundChannel.invokeMethod(Keys.BCM_INIT,
-                                    hashMapOf(Keys.ARG_INIT_CALLBACK to initCallback, Keys.ARG_INIT_DATA_CALLBACK to initialDataMap))
+                            backgroundChannel?.invokeMethod(Keys.BCM_INIT,
+                                hashMapOf(Keys.ARG_INIT_CALLBACK to initCallback, Keys.ARG_INIT_DATA_CALLBACK to initialDataMap))
                         }
             }
             isInitCallbackCalled = true
