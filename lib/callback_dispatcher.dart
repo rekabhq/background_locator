8f00532cf0a2ddd492e44d4aa12530feeec4f9b6
Ko Nagase <nagase@georepublic.co.jp>
2022-11-26 23:21:30 +0900

    Support null safety for callback handler

--- lib/callback_dispatcher.dart
+++ lib/callback_dispatcher.dart
@@ -15,11 +15,13 @@ void callbackDispatcher() {
   _backgroundChannel.setMethodCallHandler((MethodCall call) async {
     if (Keys.BCM_SEND_LOCATION == call.method) {
       final Map<dynamic, dynamic> args = call.arguments;
-      final Function callback = PluginUtilities.getCallbackFromHandle(
+      final Function? callback = PluginUtilities.getCallbackFromHandle(
           CallbackHandle.fromRawHandle(args[Keys.ARG_CALLBACK]))!;
       final LocationDto location =
           LocationDto.fromJson(args[Keys.ARG_LOCATION]);
-      callback(location);
+      if (callback != null) {
+        callback(location);
+      }
     } else if (Keys.BCM_NOTIFICATION_CLICK == call.method) {
       final Map<dynamic, dynamic> args = call.arguments;
       final Function? notificationCallback =
